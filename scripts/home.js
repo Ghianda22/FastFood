ce2fd90a7ad412e257ff43e871110890c36f7515
Ghianda22 <59868467+Ghianda22@users.noreply.github.com>
2020-06-18 01:52:13 +0200

    Starting to set index.html
    - fixed necessary links to test personal area

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,9 @@
-
 function detectUser(){
     if(sessionStorage.getItem("logged") != null){
         let user = JSON.parse(sessionStorage.getItem("logged"));
         if(user.vatNum == null){
             //customer mode
+            document.getElementById("userIcon").style.display = "inline";
             document.getElementById("userIcon").href = "pages/personalAreaC.html";
 
         }else{
